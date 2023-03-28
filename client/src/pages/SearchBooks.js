28314914b9d1831615c82a9d8f3aed6c1ef9d0bf
lileee <112982735+lileeeeeee@users.noreply.github.com>
2023-03-28 13:03:20 -0500

    updated to react-bootstrap v 1.0.1, app loads without errors now

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -91,7 +91,7 @@ const SearchBooks = () => {
         <Container>
           <h1>Search for Books!</h1>
           <Form onSubmit={handleFormSubmit}>
-            <Form.Row>
+            {/* /* <Form.Row> */}
               <Col xs={12} md={8}>
                 <Form.Control
                   name='searchInput'
@@ -107,7 +107,7 @@ const SearchBooks = () => {
                   Submit Search
                 </Button>
               </Col>
-            </Form.Row>
+            {/* </Form.Row> */}
           </Form>
         </Container>
       </div>
